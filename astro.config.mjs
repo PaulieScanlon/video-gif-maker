a9d5e8e5444b15f1512fb24692fd1d3207f60b2e
Paul Scanlon <pauliescanlon@gmail.com>
2023-11-14 17:19:10 -0500

    feat: add umd video transcoding

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
+  },
+  integrations: [react()],
 });
---
+++ src/components/video-component.jsx
@@ -0,0 +1,52 @@
+import { useState, useRef } from 'react';
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+import { toBlobURL, fetchFile } from '@ffmpeg/util';
+
+const VideoComponent = () => {
+  const [loaded, setLoaded] = useState(false);
+  const ffmpegRef = useRef(new FFmpeg());
+  const videoRef = useRef(null);
+  const messageRef = useRef(null);
+
+  const load = async () => {
+    const baseURL = 'https://unpkg.com/@ffmpeg/core-mt@0.12.4/dist/esm';
+    // const baseURL = 'https://unpkg.com/@ffmpeg/core@0.12.4/dist/umd';
+    const ffmpeg = ffmpegRef.current;
+    ffmpeg.on('log', ({ message }) => {
+      if (messageRef.current) messageRef.current.innerHTML = message;
+    });
+    // toBlobURL is used to bypass CORS issue, urls with the same
+    // domain can be used directly.
+    await ffmpeg.load({
+      // coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
+      wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
+      // workerURL: await toBlobURL(`${baseURL}/ffmpeg-core.worker.js`, 'text/javascript'),
+    });
+    setLoaded(true);
+  };
+
+  const transcode = async () => {
+    const videoURL = 'https://ffmpegwasm.netlify.app/video/video-15s.avi';
+    const ffmpeg = ffmpegRef.current;
+    await ffmpeg.writeFile('input.avi', await fetchFile(videoURL));
+    await ffmpeg.exec(['-i', 'input.avi', 'output.mp4']);
+    const fileData = await ffmpeg.readFile('output.mp4');
+    const data = new Uint8Array(fileData);
+    if (videoRef.current) {
+      videoRef.current.src = URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' }));
+    }
+  };
+
+  return loaded ? (
+    <>
+      <video ref={videoRef} controls></video>
+      <br />
+      <button onClick={transcode}>Transcode avi to mp4</button>
+      <p ref={messageRef}></p>
+    </>
+  ) : (
+    <button onClick={load}>Load ffmpeg-core</button>
+  );
+};
+
+export default VideoComponent;
